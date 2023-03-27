391cb207f4637a62fbce8d688579f22540f7858e
ylf <2872444509@qq.com>
2023-03-27 21:24:46 +0800

    CudaAllocator

--- c_plus_plus.cu
+++ c_plus_plus.cu
@@ -1,38 +1,29 @@
 #include <cstdio>
 #include <cuda_runtime.h>
 #include "helper_cuda.h"
+#include <vector>
 
-__global__ void kernel(int *ptr, int n) {
-    /**
-    \brief 低效
-    */
-    for (int i = 0; i < n; i++) {
-        ptr[i] = i;
+template <class T>
+struct CudaAllocator {
+    using value_type = T;
+    T *allocate(size_t size) {
+        T *ptr = nullptr;
+        checkCudaErrors(cudaMallocManaged(&ptr, size * sizeof(T)));
+        return ptr;
     }
-}
 
-__global__ void kernel2(int *ptr, int n) {
-    /**
-    \brief 线程数不能太多
-    */
-    if (threadIdx.x < n) {
-        ptr[threadIdx.x] = threadIdx.x;
+    void deallocate(T *ptr, size_t size = 0) {
+        checkCudaErrors(cudaFree(ptr));
     }
-}
+};
 
-__global__ void kernel3(int *ptr, int n) {
-    for (int i = threadIdx.x; i < n; i+= blockDim.x) {
-        ptr[i] = i;
-    }
-}
-
-__global__ void kernel4(int *ptr, int n) {
+__global__ void kernel(int *arr, int n) {
     for (int i = blockDim.x * blockIdx.x + threadIdx.x; i < n; i += blockDim.x * gridDim.x) {
-        ptr[i] = i;
+        arr[i] = i;
     }
 }
 
-bool test(int *ptr, int n) {
+bool test(std::vector<int, CudaAllocator<int>> &ptr, int n) {
     bool flag = true;
     for (int i = 0; i < n; i++) {
         if (ptr[i] != i) {
@@ -44,17 +35,17 @@ bool test(int *ptr, int n) {
 }
 
 int main() {
-    int *ptr, n = 65535;
-    int threadNum = 128, blockNum = (n + threadNum - 1) / threadNum;
-    checkCudaErrors(cudaMallocManaged(&ptr, n * sizeof(int)));
-    kernel4<<<blockNum, threadNum>>>(ptr, n);
+    int n = 65536;
+    std::vector<int, CudaAllocator<int>> arr(n);
+
+    kernel<<<32, 128>>>(arr.data(), n);
+
     checkCudaErrors(cudaDeviceSynchronize());
-    if (test(ptr, n)) {
-        printf("cong!\n");
+    
+    if (test(arr, n)) {
+        printf("cong.\n");
     } else {
-        printf("wrong.\n");
+        printf("failed.\n");
     }
-    // checkCudaErrors(cudaDeviceSynchronize());
-    cudaFree(ptr);
     return 0;
 }
