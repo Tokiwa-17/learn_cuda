e3fa6dd1f623d2198e04eb8ce2ad8fb5ef0901eb
ylf <2872444509@qq.com>
2023-03-27 22:17:12 +0800

    kernel template function

--- c_plus_plus.cu
+++ c_plus_plus.cu
@@ -2,6 +2,7 @@
 #include <cuda_runtime.h>
 #include "helper_cuda.h"
 #include <vector>
+#include <type_traits>
 
 template <class T>
 struct CudaAllocator {
@@ -15,15 +16,23 @@ struct CudaAllocator {
     void deallocate(T *ptr, size_t size = 0) {
         checkCudaErrors(cudaFree(ptr));
     }
+
+    // template <class ...Args>
+    // void construct(T *p, Args &&...args) {
+    //     if constexpr (!(sizeof...(Args) == 0 && std::is_pod_v<T>))
+    //         ::new((void *)p) T(std::forward<Args>(args)...);
+    // }
 };
 
-__global__ void kernel(int *arr, int n) {
-    for (int i = blockDim.x * blockIdx.x + threadIdx.x; i < n; i += blockDim.x * gridDim.x) {
+template <int N, class T>
+__global__ void kernel(T *arr) {
+    for (int i = blockDim.x * blockIdx.x + threadIdx.x; i < N; i += blockDim.x * gridDim.x) {
         arr[i] = i;
     }
 }
 
-bool test(std::vector<int, CudaAllocator<int>> &ptr, int n) {
+template <class T>
+bool test(std::vector<T, CudaAllocator<T>> &ptr, int n) {
     bool flag = true;
     for (int i = 0; i < n; i++) {
         if (ptr[i] != i) {
@@ -35,10 +44,10 @@ bool test(std::vector<int, CudaAllocator<int>> &ptr, int n) {
 }
 
 int main() {
-    int n = 65536;
+    const int n = 65536;
     std::vector<int, CudaAllocator<int>> arr(n);
 
-    kernel<<<32, 128>>>(arr.data(), n);
+    kernel<n><<<32, 128>>>(arr.data());
 
     checkCudaErrors(cudaDeviceSynchronize());
     
